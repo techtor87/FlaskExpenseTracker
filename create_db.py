baff17c382b46aa01a48c67fcc9d793b081f30a4
Greg Faulconbridge <gfaulconbridge@gmail.com>
2024-05-03 16:17:46 -0400

    stuff

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 from application import app
 from application import db
-from application.models import Category, Transactions, Account, Rules
+from application.models import IncomeExpenses, Account
 
 with app.app_context():
     db.create_all()
